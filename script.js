b4de7e1c13b20de1aca61b05db9cf0daadf6001a
Slon928 <dudinskiyav@gmail.com>
2025-06-03 22:11:45 +0300

    sixth

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.getElementById('goButton').onclick=function()
 {
     location.href='index.html';
 };
+document.getElementById('goButton').addEventListener('click', function()
+{
+const phone=document.getElementById('n2').value;
+const names=document.getElementById('n1').value;
+const city=document.getElementById('n3').value;
+const email=document.getElementById('n4').value;
+localStorage.setItem('phone',phone);
+localStorage.setItem('names',names);
+localStorage.setItem('city',city);
+localStorage.setItem('email',email);
+window.location.href='index.html';
+});
+
